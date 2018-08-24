783bd25db040cfcb8f611829d6f3b4b53541206b
Filip Kuszenin <filipkuszenin@gmail.com>
2018-08-24 18:04:45 +0100

    .save() for PersonalTrainer created

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,8 +14,8 @@ personaltrainer2 = PersonalTrainer.new({
   'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 });
 
-# personaltrainer1.save()
-# personaltrainer2.save()
+personaltrainer1.save()
+personaltrainer2.save()
 
 member1 = Member.new({
   'name' => 'Karim Benzema',
--- models/personaltrainer.rb
+++ models/personaltrainer.rb
@@ -1,3 +1,5 @@
+require_relative('../db/sql_runner')
+
 class PersonalTrainer
   attr_accessor(:name, :description)
   attr_reader(:id)
@@ -7,5 +9,18 @@ class PersonalTrainer
     @name = options['name']
     @description = options['description']
   end
+
+
+
+  def save()
+    sql = "INSERT INTO personaltrainers
+          (name, description)
+          VALUES
+          ($1, $2) RETURNING *"
+    values = [@name, @description]
+    result = SqlRunner.run(sql,values)
+    @id = result[0]['id'].to_i
+  end
+
 
 end
