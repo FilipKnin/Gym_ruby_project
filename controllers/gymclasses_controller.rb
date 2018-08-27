9183625b4a2e6f21f08ec42525e53ec1dded806b
Filip Kuszenin <filipkuszenin@gmail.com>
2018-08-27 16:30:46 +0100

    show.erb for GymClass updated. Bug fixed(not displaying page when there is no members  assigned to GymClass)

--- controllers/gymclasses_controller.rb
+++ controllers/gymclasses_controller.rb
@@ -4,12 +4,12 @@ require_relative('../models/gymclass')
 also_reload('../models/*')
 
 
-# get '/gymclasses' do
-#   @gymclasses = GymClass.all()
-#   erb(:'gymclasses/index')
-# end
-#
-# get '/gymclasses/:id' do
-#   @gymclass = GymClass.find_by_id(params['id'].to_i())
-#   erb(:'gymclasses/show')
-# end
+get '/gymclasses' do
+  @gymclasses = GymClass.all()
+  erb(:'gymclasses/index')
+end
+
+get '/gymclasses/:id' do
+  @gymclass = GymClass.find_by_id(params['id'].to_i())
+  erb(:'gymclasses/show')
+end
