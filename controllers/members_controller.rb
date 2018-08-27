456c38ea85942f9bd51b318100c931307e7c0e10
Filip Kuszenin <filipkuszenin@gmail.com>
2018-08-27 17:05:59 +0100

    Form to add new member created

--- controllers/members_controller.rb
+++ controllers/members_controller.rb
@@ -9,12 +9,11 @@ get '/members' do
   erb (:'members/index')
 end
 
+get '/members/new' do
+  erb(:'members/new')
+end
 
 get '/members/:id' do
   @member = Member.find_by_id(params['id'].to_i())
   erb(:'members/show')
 end
-
-# get '/members/new' do
-#   erb(:'members/new')
-# end
