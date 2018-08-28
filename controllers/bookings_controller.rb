9ab7d58236dc9d66a4293328b33d5b7ae15cb8eb
Filip Kuszenin <filipkuszenin@gmail.com>
2018-08-28 23:32:59 +0100

    get '/bookings/new' created and tested (not posting data yet)

--- controllers/bookings_controller.rb
+++ controllers/bookings_controller.rb
@@ -1,8 +1,16 @@
 require('sinatra')
 require('sinatra/contrib/all')
 require_relative('../models/booking')
+require_relative('../models/gymclass')
+require_relative('../models/member')
 also_reload('../models/*')
 
 get '/bookings' do
   erb(:'bookings/index')
+end
+
+get '/bookings/new' do
+  @gymclasses = GymClass.all()
+  @members = Member.all()
+  erb(:'bookings/new')
 end
